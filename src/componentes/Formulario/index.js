6ac5d9676ac611137cd6bb3f2d7d97c1434947b5
Caroline Faria <caroline.gdfaria@gmail.com>
2023-07-09 18:59:59 -0300

    :sparkles: feat: criação do botão de enviar card

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,14 +1,39 @@
 import './Formulario.css'
 import CampoTexto from "../CampoTexto";
+import ListaSuspensa from "../ListaSuspensa";
+import Botao from "../Botao";
 
 const Formulario = () => {
+
+    const aldeias = [
+        "Folha",
+        "Areia",
+        "Névoa",
+        "Nuvem",
+        "Pedra",
+        "Névoa Sangrenta"
+    ]
+
+    const niveis = [
+        "Jonin",
+        "Chunin",
+        "Genin",
+        "ANBU",
+        "Sannin",
+        "Kage"
+    ]
+
     return (
         <section className="formulario">
             <form>
                 <h2>Preencha os dados para criar o card do ninja</h2>
-                <CampoTexto label="Nome" placeholder="Digite o seu nome"/>
-                <CampoTexto label="Classe" placeholder="Digite o nome da sua classe"/>
-                <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
+                <CampoTexto label="Nome" placeholder="Digite o seu nome" />
+                <ListaSuspensa label="Nivel" itens={niveis} />
+                <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
+                <ListaSuspensa label="Aldeia" itens={aldeias} />
+                <Botao>
+                    Criar card
+                </Botao>
             </form>
         </section>
     )
